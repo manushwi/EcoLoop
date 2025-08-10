73885411e94f30f1f42d71025c431b822d3a800e
Anurag <anuragsingh2006133@gmail.com>
2025-08-10 20:26:11 +0530

    adding-landing-page/upload-page

--- public/js/upload.js
+++
@@ -1,5 +0,0 @@
-function toggleTheme() {
-    const html = document.documentElement;
-    const currentTheme = html.getAttribute('data-theme');
-    html.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark');
-}
